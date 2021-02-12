728f4be170d7636ace1394dea463e074d642f87d
Kazi Minhaj <kaziminhajulhaider@gmail.com>
2021-02-12 12:35:58 +0600

    null-undefined

---
+++ null-undefined.js
@@ -0,0 +1,35 @@
+// -----------type1---------------
+// let name;
+// console.log(name);
+
+// -------------type2----------------
+// function add(a, b) {
+//     console.log(a + b);
+// }
+
+// const result = add(2, 3);
+// console.log(result);
+
+// =========type3========
+
+// function add(a, b) {
+//     console.log(a, b);
+// }
+
+// add(2);
+
+// <----------------type4--------------->
+// const person = {
+//     name: "kazi",
+//     age: 24
+// }
+// console.log(person.kazi);
+
+
+// type5
+
+// let ages = [2, 3, 4];
+// console.log(ages[5]);
+
+// ei rokom aro onek bhabe amra undefined paite pari.
+//null ->>> null amra normally set kori. say amra kono ekta object er property null korte pari
